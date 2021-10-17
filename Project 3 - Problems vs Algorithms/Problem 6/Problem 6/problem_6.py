776f2eeb125766cc54b19febad8283c6a0159c49
Somanath-KC <somanath.chowdary@ymail.com>
2021-10-17 07:39:28 +0530

    Complete Problem 6

--- Project 3 - Problems vs Algorithms/Problem 6/Problem 6/problem_6.py
+++ Project 3 - Problems vs Algorithms/Problem 6/Problem 6/problem_6.py
@@ -1,16 +1,54 @@
-def get_min_max(ints):
+import random
+
+
+def get_min_max(ints=[]):
     """
     Return a tuple(min, max) out of list of unsorted integers.
 
     Args:
        ints(list): list of integers containing one or more integers
     """
-    pass
+    if not (ints is None) and (len(ints) > 0):
+        min, max = ints[0], ints[0]
+    else:
+        return ()
 
-## Example Test Case of Ten Integers
-import random
+    for i in ints:
+        if i < min:
+            min = i
+        if i > max:
+            max = i
+    return min, max
 
+
+#####     #####
+#   Testing   #
+#####     #####
+
+# Test Case 1
+print("\n", "#"*8, " - Test Case 1 [General Working] - ", "#"*8, "\n")
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
+print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
 
-print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+# Test Case 2
+print("\n", "#"*8, " - Test Case 2 [Single Element] - ", "#"*8, "\n")
+l = [1]
+print("Pass" if ((1, 1) == get_min_max(l)) else "Fail")
+
+# Test Case 3
+print("\n", "#"*8, " - Test Case 3 [Empty Input] - ", "#"*8, "\n")
+l = None
+print("Pass" if (() == get_min_max(l)) else "Fail")
+
+# Test Case 4
+print("\n", "#"*8, " - Test Case 4 [Large Numbers] - ", "#"*8, "\n")
+l = [i for i in range(10000, 999999)]  # a list containing -Large Numbers
+random.shuffle(l)
+print("Pass" if ((10000, 999998) == get_min_max(l)) else "Fail")
+
+# Test Case 5
+print("\n", "#"*8, " - Test Case 5 [Negative Numbers] - ", "#"*8, "\n")
+l = [i for i in range(-25, 0)]  # a list containing -25 to -1
+random.shuffle(l)
+print("Pass" if ((-25, -1) == get_min_max(l)) else "Fail")
