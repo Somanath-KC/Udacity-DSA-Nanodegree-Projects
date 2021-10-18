078aeea841cdfd8abe4c1dd3d0d1289e0d331d6c
Somanath-KC <somanath.chowdary@ymail.com>
2021-10-18 17:40:04 +0530

    Add PEP8 Style

--- Project 3 - Problems vs Algorithms/Problem 1/problem_1.py
+++ Project 3 - Problems vs Algorithms/Problem 1/problem_1.py
@@ -9,14 +9,14 @@ def sqrt(number):
     """
     if type(number) != int or number < 0:
         return
-    
+
     if number in [0, 1]:
         return number
-    
+
     left = 1
-    right = number
+    right = number//2  # Becase square root of n is always less than n/2
     middle = (left + right)//2
-    
+
     while right >= left:
         if middle*middle == number:
             return middle
@@ -25,7 +25,7 @@ def sqrt(number):
         else:
             right = middle-1
         middle = (left + right)//2
-    
+
     return middle
 
 
@@ -35,23 +35,23 @@ def sqrt(number):
 
 # Test Case 1
 print("\n", "#"*8, " - Test Case 1 [General Working] - ", "#"*8, "\n")
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
-print ("Pass" if  (4 == sqrt(24)) else "Fail")
+print("Pass" if (3 == sqrt(9)) else "Fail")
+print("Pass" if (4 == sqrt(16)) else "Fail")
+print("Pass" if (1 == sqrt(1)) else "Fail")
+print("Pass" if (5 == sqrt(27)) else "Fail")
+print("Pass" if (4 == sqrt(24)) else "Fail")
 
 
 # Test Case 2
 print("\n", "#"*8, " - Test Case 2 [Negative Values] - ", "#"*8, "\n")
-print ("Pass" if  (None == sqrt(-9)) else "Fail")
+print("Pass" if (None is sqrt(-9)) else "Fail")
 
 
 # Test Case 3
 print("\n", "#"*8, " - Test Case 3 [Zero value] - ", "#"*8, "\n")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
+print("Pass" if (0 == sqrt(0)) else "Fail")
 
 
 # Test Case 4
 print("\n", "#"*8, " - Test Case 4 [Null value] - ", "#"*8, "\n")
-print ("Pass" if  (None == sqrt(None)) else "Fail")
+print("Pass" if (None is sqrt(None)) else "Fail")
