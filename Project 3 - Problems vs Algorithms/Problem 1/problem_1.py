228479ee4dd815aec3064491c8934b5dd6c8f1fd
Somanath-KC <somanath.chowdary@ymail.com>
2021-10-18 17:04:58 +0530

    Complete Problem 1

---
+++ Project 3 - Problems vs Algorithms/Problem 1/problem_1.py
@@ -0,0 +1,57 @@
+def sqrt(number):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+    if type(number) != int or number < 0:
+        return
+    
+    if number in [0, 1]:
+        return number
+    
+    left = 1
+    right = number
+    middle = (left + right)//2
+    
+    while right >= left:
+        if middle*middle == number:
+            return middle
+        elif middle*middle < number:
+            left = middle+1
+        else:
+            right = middle-1
+        middle = (left + right)//2
+    
+    return middle
+
+
+#####     #####
+#   Testing   #
+#####     #####
+
+# Test Case 1
+print("\n", "#"*8, " - Test Case 1 [General Working] - ", "#"*8, "\n")
+print ("Pass" if  (3 == sqrt(9)) else "Fail")
+print ("Pass" if  (4 == sqrt(16)) else "Fail")
+print ("Pass" if  (1 == sqrt(1)) else "Fail")
+print ("Pass" if  (5 == sqrt(27)) else "Fail")
+print ("Pass" if  (4 == sqrt(24)) else "Fail")
+
+
+# Test Case 2
+print("\n", "#"*8, " - Test Case 2 [Negative Values] - ", "#"*8, "\n")
+print ("Pass" if  (None == sqrt(-9)) else "Fail")
+
+
+# Test Case 3
+print("\n", "#"*8, " - Test Case 3 [Zero value] - ", "#"*8, "\n")
+print ("Pass" if  (0 == sqrt(0)) else "Fail")
+
+
+# Test Case 4
+print("\n", "#"*8, " - Test Case 4 [Null value] - ", "#"*8, "\n")
+print ("Pass" if  (None == sqrt(None)) else "Fail")
